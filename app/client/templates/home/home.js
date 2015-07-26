f48fd38c1ce8827133fa0b12615b8234061b76de
James Hatfield <jhat.home@gmail.com>
2015-07-26 16:01:37 -0500

    first famous integration commit, stored divs rendered with click handlers.

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -35,9 +35,52 @@ Template.Home.created = function () {
 
 Template.Home.rendered = function () {
 
-	var $canvas = $('.canvas')
+Famous.Engine.init(); //note our NAMESPACED function name
+ 
+ var scene = Famous.Engine.createScene();
+ var appNode = scene.addChild()
+ var headerNode = appNode.addChild()
+ var contentNode = appNode.addChild()
+ var footerNode = appNode.addChild()
+
+
+
+ 
+ 
+//ALL THE FOLLOWING CODE IS A DIRECT COPY FROM 
+//FAMOUS CODE EXCEPT FUNCTION NAMES
+ 
 	var els = Elements.find({'room': Session.get('currentRoom')}).fetch()
 	console.log('els', els)
+	_.each(els, function(el, i){
+		var div = contentNode.addChild()
+		div
+		.setSizeMode('absolute', 'absolute')
+        .setAbsoluteSize(200, 100)
+        //.setAlign(0,0,0)
+        //.setMountPoint(0,0)
+        //.setOrigin(0, 0,0)
+        .setPosition((i*210), el.y)
+
+        div.content = new Famous.DOMElement(div, {    
+	//note the use of our namespaced function
+			id: el._id,
+	        content: el._id,
+	        classes: ['div']
+	    })
+
+	    // Listen for click event 
+		div.addUIEvent('click')
+
+		// Add an onReceive method that catches all UI events
+		div.onReceive = function(event, payload){
+		    if(event==='click'){
+		    	console.log('hello', this)
+		       this.content.setContent('I\'ve been clicked')
+		    }
+		}
+	})
+
 
 };
 
